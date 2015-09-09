bf198f48d839522f5b7707b730ea42e410145938
Yuki Ito <yuki@gnnk.net>
2015-09-09 18:04:02 +0900

    tmp

--- lib/fluent/plugin/buf_ephemeral.rb
+++ lib/fluent/plugin/buf_ephemeral.rb
@@ -1,10 +1,26 @@
 module Fluent
   class EphemeralBuffer < MemoryBuffer
     Fluent::Plugin.register_buffer('ephemeral', self)
+
+    config_param :buffer_log_level, :string, :default => 'info'
+
+    def configure(conf)
+      super
+
+      begin
+        Fluent::Log.str_to_level(@buffer_log_level)
+      rescue
+        raise ConfigError "Invalid buffer log level '#{@buffer_lg_level}'"
+      end
+
+      $log.warn "If failed to write a chunk once, it will be lost."
+    end
     
     def write_chunk(chunk, out)
       super
-    rescue
+    rescue => e
+      $log.send(@buffer_log_level, "failed to write the chunk", chunk_key: chunk.key)
+      $log.send("#{@buffer_log_level}_backtrace", e.backtrace)
     end
   end
 end
