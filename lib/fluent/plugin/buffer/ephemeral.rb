84264e151869a9dcc969dba42b4f1b095fbe9f6d
Yuki Ito <yuki@gnnk.net>
2015-09-09 16:45:45 +0900

    Initial commit

---
+++ lib/fluent/plugin/buffer/ephemeral.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/buffer/ephemeral/version"
+
+module Fluent
+  module Plugin
+    module Buffer
+      module Ephemeral
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/fluent/plugin/buffer/ephemeral/version.rb
@@ -0,0 +1,9 @@
+module Fluent
+  module Plugin
+    module Buffer
+      module Ephemeral
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
